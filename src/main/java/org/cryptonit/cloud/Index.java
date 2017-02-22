63de4b0b867e352e7531499138bfa28a79bca25a
Mathias Brossard <mathias@brossard.org>
2017-02-22 01:55:25 -0800

    Serve index file directly from servlet

--- src/main/java/org/cryptonit/cloud/Index.java
+++ src/main/java/org/cryptonit/cloud/Index.java
@@ -1,10 +1,13 @@
 package org.cryptonit.cloud;
 
+import java.io.File;
 import java.net.URI;
 import java.net.URISyntaxException;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import static org.cryptonit.cloud.Service.WEBAPP_RESOURCES_LOCATION;
 
 /**
  * @author Mathias Brossard
@@ -14,7 +17,7 @@ public class Index {
 
     @GET
     public Response index() throws URISyntaxException {
-        URI index = new URI("index.html");
-        return Response.temporaryRedirect(index).build();
+        URI uri = Thread.currentThread().getContextClassLoader().getResource(WEBAPP_RESOURCES_LOCATION + "/index.html").toURI();
+        return Response.ok(new File(uri), MediaType.TEXT_HTML).build();
     }
 }
