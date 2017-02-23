6ace25a2e71997d1a00b1cda25bd9a43dfd3e806
Mathias Brossard <mathias@brossard.org>
2017-02-23 02:45:24 -0800

    Disable Flyway debug logging

--- src/main/java/org/cryptonit/cloud/Database.java
+++ src/main/java/org/cryptonit/cloud/Database.java
@@ -2,6 +2,10 @@ package org.cryptonit.cloud;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import org.flywaydb.core.internal.util.logging.Log;
+import org.flywaydb.core.internal.util.logging.LogCreator;
+import org.flywaydb.core.internal.util.logging.LogFactory;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author Mathias Brossard
@@ -9,6 +13,19 @@ import java.sql.DriverManager;
 public class Database {
 
     public Database(String url, String user, String password) {
+        class ourSlf4jLogCreator implements LogCreator {
+
+            public Log createLogger(Class<?> clazz) {
+                org.slf4j.Logger l = LoggerFactory.getLogger(clazz);
+                if (l instanceof ch.qos.logback.classic.Logger) {
+                    ch.qos.logback.classic.Logger logbackLogger = (ch.qos.logback.classic.Logger) l;
+                    logbackLogger.setLevel(ch.qos.logback.classic.Level.INFO);
+                }
+                return new org.flywaydb.core.internal.util.logging.slf4j.Slf4jLog(l);
+            }
+        }
+        LogFactory.setLogCreator(new ourSlf4jLogCreator());
+
         Connection c = null;
         try {
             Class.forName("org.postgresql.Driver");
