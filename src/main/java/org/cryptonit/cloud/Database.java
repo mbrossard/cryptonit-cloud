1baf3b827c5372e82c9f9706a12f3391de3019d4
Mathias Brossard <mathias@brossard.org>
2017-02-23 02:37:22 -0800

    Load postgresql driver and attempt connection

--- src/main/java/org/cryptonit/cloud/Database.java
+++ src/main/java/org/cryptonit/cloud/Database.java
@@ -1,10 +1,19 @@
 package org.cryptonit.cloud;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 /**
  * @author Mathias Brossard
  */
 public class Database {
 
     public Database(String url, String user, String password) {
+        Connection c = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager.getConnection(url, user, password);
+        } catch (Exception e) {
+        }
     }
 }
