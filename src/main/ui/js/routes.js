4cf1ef1d64669368003e1268df6b7573c68e3b73
Mathias Brossard <mathias@brossard.org>
2017-02-27 09:56:31 -0800

    Adding a route

--- src/main/ui/js/routes.js
+++ src/main/ui/js/routes.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
-import { Router, browserHistory } from 'react-router';
+import { Router, Route, browserHistory } from 'react-router';
+
+import Layout from './layout/layout.js'
 
 export default class Routes extends Component {
   render() {
     return (
       <Router history={ browserHistory }>
+        <Route path="/" component={Layout}>
+        </Route>
       </Router>
     );
   }
