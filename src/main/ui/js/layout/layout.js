5278fb254bdde510a5f3e1a9d23a35bb43c01bb3
Mathias Brossard <mathias@brossard.org>
2017-03-01 21:51:28 -0800

    Add missing layout.js

---
+++ src/main/ui/js/layout/layout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+let Layout = React.createClass({
+    render: function(){
+        return (
+            <div>
+            </div>
+        )
+    }
+});
+
+export default Layout
