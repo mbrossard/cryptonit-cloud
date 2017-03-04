e0d27d112e11e85c52ee08eb1db865b486a8a353
Mathias Brossard <mathias@brossard.org>
2017-03-04 08:15:02 -0800

    Key generation

--- src/test/java/org/cryptonit/cloud/timestamping/Authority.java
+++ src/test/java/org/cryptonit/cloud/timestamping/Authority.java
@@ -1,8 +1,28 @@
 package org.cryptonit.cloud.timestamping;
 
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.SecureRandom;
+
 /**
  * @author Mathias Brossard
  */
 public class Authority {
-    
+    KeyPair generateKeypair(int size) {
+        SecureRandom rand = new SecureRandom();
+        KeyPairGenerator pairGenerator = null;
+        try {
+            pairGenerator = KeyPairGenerator.getInstance("RSA", "BC");
+            pairGenerator.initialize(size, rand);
+            KeyPair kp = pairGenerator.generateKeyPair();
+        return kp;
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace(System.err);
+        } catch (NoSuchProviderException e) {
+            e.printStackTrace(System.err);
+        }
+        return null;
+    }    
 }
