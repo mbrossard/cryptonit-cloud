5ba7c14cc9bb73162d78dfcb19288cfd5579f77c
Mathias Brossard <mathias@brossard.org>
2017-03-09 09:15:47 -0800

    Getting started on TSA

--- src/main/java/org/cryptonit/cloud/timestamping/Authority.java
+++ src/main/java/org/cryptonit/cloud/timestamping/Authority.java
@@ -1,7 +1,17 @@
 package org.cryptonit.cloud.timestamping;
 
+import org.bouncycastle.util.Store;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.security.*;
+import java.security.cert.X509Certificate;
 /**
  * @author Mathias Brossard
  */
 public class Authority {
+    private static Logger LOGGER;
+        
+    public Authority(KeyPair key, X509Certificate crt, Store certs) {
+        LOGGER = LoggerFactory.getLogger(Authority.class);
+    }
 }
