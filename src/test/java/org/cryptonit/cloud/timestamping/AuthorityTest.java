2b29b61ddfac4dc1b10a36cf65d782e1eabb0e11
Mathias Brossard <mathias@brossard.org>
2017-03-10 08:09:12 -0800

    Build certificate store

--- src/test/java/org/cryptonit/cloud/timestamping/AuthorityTest.java
+++ src/test/java/org/cryptonit/cloud/timestamping/AuthorityTest.java
@@ -9,6 +9,7 @@ import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.SecureRandom;
 import java.security.Security;
+import java.security.cert.CertificateEncodingException;
 import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.Date;
@@ -18,7 +19,9 @@ import org.bouncycastle.asn1.x509.ExtendedKeyUsage;
 import org.bouncycastle.asn1.x509.Extension;
 import org.bouncycastle.asn1.x509.KeyPurposeId;
 import org.bouncycastle.asn1.x509.X509Name;
+import org.bouncycastle.cert.jcajce.JcaCertStore;
 import org.bouncycastle.cert.jcajce.JcaX509ExtensionUtils;
+import org.bouncycastle.util.Store;
 import org.bouncycastle.x509.X509V3CertificateGenerator;
 import org.junit.Assert;
 import org.junit.Test;
@@ -115,6 +118,13 @@ public class AuthorityTest {
         List certList = new ArrayList();
         certList.add(caCRT);
         certList.add(tsaCRT);
+
+        Store certs = null;
+        try {
+            certs = new JcaCertStore(certList);
+        } catch (CertificateEncodingException e) {
+            e.printStackTrace();
+        }
             
         Assert.assertEquals(1, 1);
     }
