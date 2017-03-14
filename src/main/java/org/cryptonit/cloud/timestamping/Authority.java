99ca8db451d0525b76a939e304a43b9080104ce3
Mathias Brossard <mathias@brossard.org>
2017-03-13 21:37:48 -0700

    Add serial number incrementation method

--- src/main/java/org/cryptonit/cloud/timestamping/Authority.java
+++ src/main/java/org/cryptonit/cloud/timestamping/Authority.java
@@ -1,20 +1,29 @@
 package org.cryptonit.cloud.timestamping;
 
+import java.math.BigInteger;
+import java.security.*;
+import java.security.cert.X509Certificate;
 import org.bouncycastle.tsp.*;
 import org.bouncycastle.util.Store;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import java.security.*;
-import java.security.cert.X509Certificate;
+
 /**
  * @author Mathias Brossard
  */
 public class Authority {
+    private final BigInteger one = new BigInteger("1");
     private static Logger LOGGER;
     KeyPair key;
     X509Certificate crt;
     Store certs;
     BigInteger serial;
+
+    synchronized private BigInteger getNextSerial() {
+        BigInteger s = serial;
+        serial.add(one);
+        return s;
+    }
 
     public TimeStampResponse timestamp(TimeStampRequest request) {
         TimeStampResponse response = null;
