55319fdef17066534f9a071a47b2b15c6558203c
Mathias Brossard <mathias@brossard.org>
2017-03-20 20:42:17 -0700

    Add static method to set TimestampingAuthorityFactory

--- src/main/java/org/cryptonit/cloud/timestamping/Application.java
+++ src/main/java/org/cryptonit/cloud/timestamping/Application.java
@@ -1,11 +1,16 @@
 package org.cryptonit.cloud.timestamping;
 
 import javax.ws.rs.Path;
+import org.cryptonit.cloud.interfaces.TimestampingAuthorityFactory;
 
 /**
  * @author Mathias Brossard
  */
 @Path("/timestamp")
 public class Application {
+    private static TimestampingAuthorityFactory tsaFactory = null;
 
+    public static void setTimestampingAuthorityFactory(TimestampingAuthorityFactory factory) {
+        tsaFactory = factory;
+    }
 }
