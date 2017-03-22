cacb032fc46d76632d0384fe235c37c47e899291
Mathias Brossard <mathias@brossard.org>
2017-03-22 02:09:27 -0700

    Add content types to POST

--- src/main/java/org/cryptonit/cloud/timestamping/Application.java
+++ src/main/java/org/cryptonit/cloud/timestamping/Application.java
@@ -2,8 +2,10 @@ package org.cryptonit.cloud.timestamping;
 
 import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Response;
@@ -24,6 +26,8 @@ public class Application {
     private static TimestampingAuthorityFactory tsaFactory = null;
 
     @POST
+    @Consumes("application/timestamp-query")
+    @Produces("application/timestamp-reply")
     public Response timestamp(@Context HttpServletRequest request,
             @Context HttpHeaders headers) throws IOException, TSPException {
         TimeStampRequest tsq = new TimeStampRequest(request.getInputStream());
