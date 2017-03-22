45cc2ba21fd09be7d116d7a081bea3fc410a97ca
Mathias Brossard <mathias@brossard.org>
2017-03-22 02:07:08 -0700

    Add POST method

--- src/main/java/org/cryptonit/cloud/timestamping/Application.java
+++ src/main/java/org/cryptonit/cloud/timestamping/Application.java
@@ -1,6 +1,12 @@
 package org.cryptonit.cloud.timestamping;
 
+import java.io.IOException;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.Response;
 import org.cryptonit.cloud.interfaces.TimestampingAuthorityFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,6 +18,12 @@ import org.slf4j.LoggerFactory;
 public class Application {
     private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
     private static TimestampingAuthorityFactory tsaFactory = null;
+
+    @POST
+    public Response timestamp(@Context HttpServletRequest request,
+            @Context HttpHeaders headers) throws IOException {
+        return Response.status(Response.Status.NOT_FOUND).build();
+    }
 
     public static void setTimestampingAuthorityFactory(TimestampingAuthorityFactory factory) {
         tsaFactory = factory;
