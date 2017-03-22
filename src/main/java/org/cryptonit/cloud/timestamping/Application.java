3fa82b074adce137dde1fab4829dc79aef319e21
Mathias Brossard <mathias@brossard.org>
2017-03-22 01:58:44 -0700

    Add logger

--- src/main/java/org/cryptonit/cloud/timestamping/Application.java
+++ src/main/java/org/cryptonit/cloud/timestamping/Application.java
@@ -2,12 +2,15 @@ package org.cryptonit.cloud.timestamping;
 
 import javax.ws.rs.Path;
 import org.cryptonit.cloud.interfaces.TimestampingAuthorityFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author Mathias Brossard
  */
 @Path("/timestamp")
 public class Application {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
     private static TimestampingAuthorityFactory tsaFactory = null;
 
     public static void setTimestampingAuthorityFactory(TimestampingAuthorityFactory factory) {
