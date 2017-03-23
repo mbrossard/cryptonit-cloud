562b110c285c61f533682c1b60a9b704ad6bad7b
Mathias Brossard <mathias@brossard.org>
2017-03-22 23:36:59 -0700

    Add KeyParameters enum

--- src/main/java/org/cryptonit/cloud/interfaces/KeyStore.java
+++ src/main/java/org/cryptonit/cloud/interfaces/KeyStore.java
@@ -4,6 +4,20 @@ import java.security.PrivateKey;
 import java.security.PublicKey;
 
 public interface KeyStore {
+    public enum KeyParameters {
+        RSA_2048("RSA", null, 2048),
+
+        final private String algorithm;
+        final private String parameter;
+        final private int size;
+
+        KeyParameters(String algorithm, String parameter, int size) {
+            this.algorithm = algorithm;
+            this.parameter = parameter;
+            this.size = size;
+        }
+    };
+
     PrivateKey getPrivateKey(String domain, String keyIdentifier);
 
     PublicKey getPublicKey(String domain, String keyIdentifier);
