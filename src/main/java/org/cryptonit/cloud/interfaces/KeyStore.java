a33bf7fb3113ba4ab48b01d711813258819e9f54
Mathias Brossard <mathias@brossard.org>
2017-03-23 00:00:40 -0700

    Additional KeyParameters constants

--- src/main/java/org/cryptonit/cloud/interfaces/KeyStore.java
+++ src/main/java/org/cryptonit/cloud/interfaces/KeyStore.java
@@ -6,6 +6,10 @@ import java.security.PublicKey;
 public interface KeyStore {
     public enum KeyParameters {
         RSA_2048("RSA", null, 2048),
+        RSA_4096("RSA", null, 4096),
+        EC_P256("EC", "P-256", 256),
+        EC_P384("EC", "P-384", 384),
+        EC_P521("EC", "P-521", 521);
 
         final private String algorithm;
         final private String parameter;
