72cc3a604e15e12ae2cad9d73a1396076f7ae1d8
Mathias Brossard <mathias@brossard.org>
2017-03-24 01:22:09 -0700

    Implement RSA key generation

--- src/main/java/org/cryptonit/cloud/keystore/SqlKeyStore.java
+++ src/main/java/org/cryptonit/cloud/keystore/SqlKeyStore.java
@@ -1,11 +1,20 @@
 package org.cryptonit.cloud.keystore;
 
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.MessageDigest;
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.security.SecureRandom;
 import org.cryptonit.cloud.Database;
 import org.cryptonit.cloud.interfaces.KeyStore;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SqlKeyStore implements KeyStore {
+    private static final Logger LOGGER = LoggerFactory.getLogger(SqlKeyStore.class);
+    private static final SecureRandom random = new SecureRandom();
     Database database;
 
     public SqlKeyStore(Database database) throws Exception {
@@ -13,7 +22,23 @@ public class SqlKeyStore implements KeyStore {
     }
 
     private String generateKeyPair(String domain, KeyParameters params) throws Exception {
+        KeyFactory keyFactory;
+        KeyPair kp;
+        if(params.getAlgorithm().equalsIgnoreCase("RSA")) {
+            keyFactory = KeyFactory.getInstance("RSA", "BC");
+            KeyPairGenerator rsaKeyPairGenerator = KeyPairGenerator.getInstance("RSA", "BC");
+            rsaKeyPairGenerator.initialize(params.getSize(), random);
+            kp = rsaKeyPairGenerator.generateKeyPair();
+        } else {
             return null;
+        }
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        md.update(kp.getPublic().getEncoded());
+        byte[] digest = md.digest();
+
+        String keyId = String.format("%064x", new java.math.BigInteger(1, digest));
+
+        return keyId;
     }
     
     @Override
