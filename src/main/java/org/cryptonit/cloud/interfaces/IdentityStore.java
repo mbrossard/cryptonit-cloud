ecabdb817a39172f5de21ba91ded66c57782be7d
Mathias Brossard <mathias@brossard.org>
2017-03-27 23:46:38 -0700

    Add first method to IdentityStore interface

--- src/main/java/org/cryptonit/cloud/interfaces/IdentityStore.java
+++ src/main/java/org/cryptonit/cloud/interfaces/IdentityStore.java
@@ -1,5 +1,7 @@
 package org.cryptonit.cloud.interfaces;
 
+import org.bouncycastle.asn1.x500.X500Name;
+
 public interface IdentityStore {
-    
+    public String newIdentity(String domain, String keyId, X500Name subject) throws Exception;
 }
