49de292103517d33b8bb89b0ddc8739d516a7948
Mathias Brossard <mathias@brossard.org>
2017-03-31 00:57:48 -0700

    Adding logger

--- src/main/java/org/cryptonit/cloud/keystore/SqlIdentityStore.java
+++ src/main/java/org/cryptonit/cloud/keystore/SqlIdentityStore.java
@@ -4,8 +4,11 @@ import org.bouncycastle.asn1.x500.X500Name;
 import org.cryptonit.cloud.Database;
 import org.cryptonit.cloud.interfaces.IdentityStore;
 import org.cryptonit.cloud.interfaces.KeyStore;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SqlIdentityStore implements IdentityStore {
+    private static final Logger LOGGER = LoggerFactory.getLogger(SqlIdentityStore.class);
     Database database;
     KeyStore keyStore;
 
