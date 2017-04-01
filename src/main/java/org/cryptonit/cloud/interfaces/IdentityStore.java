1f3fbb5dcd81a38418fa2717a773c6afe58f35fc
Mathias Brossard <mathias@brossard.org>
2017-04-01 08:20:43 -0700

    Add interface to set certificate value

--- src/main/java/org/cryptonit/cloud/interfaces/IdentityStore.java
+++ src/main/java/org/cryptonit/cloud/interfaces/IdentityStore.java
@@ -1,7 +1,11 @@
 package org.cryptonit.cloud.interfaces;
 
+import javax.security.cert.X509Certificate;
 import org.bouncycastle.asn1.x500.X500Name;
 
 public interface IdentityStore {
+
     public String newIdentity(String domain, String keyId, X500Name subject) throws Exception;
+
+    public boolean setCertificate(String domain, String identityId, X509Certificate certificate) throws Exception;
 }
