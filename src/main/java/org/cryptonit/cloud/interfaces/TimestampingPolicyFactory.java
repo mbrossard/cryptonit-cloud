79b0f6da8af107712cced7b1e057c3fb98421aff
Mathias Brossard <mathias@brossard.org>
2017-04-08 22:32:49 -0700

    Add additional getTimestampingPolicy method

--- src/main/java/org/cryptonit/cloud/interfaces/TimestampingPolicyFactory.java
+++ src/main/java/org/cryptonit/cloud/interfaces/TimestampingPolicyFactory.java
@@ -1,6 +1,10 @@
 package org.cryptonit.cloud.interfaces;
 
+import org.bouncycastle.asn1.ASN1ObjectIdentifier;
+
 public interface TimestampingPolicyFactory {
 
     TimestampingPolicy getTimestampingPolicy(String domain);
+
+    TimestampingPolicy getTimestampingPolicy(String domain, ASN1ObjectIdentifier policyId);
 }
