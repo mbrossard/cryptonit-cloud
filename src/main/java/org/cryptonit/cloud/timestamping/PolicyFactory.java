96fdf8962d1dc006e7d5bb52d69d526feec86f7f
Mathias Brossard <mathias@brossard.org>
2017-04-10 22:03:24 -0700

    Begin PolicyFactory class

---
+++ src/main/java/org/cryptonit/cloud/timestamping/PolicyFactory.java
@@ -0,0 +1,11 @@
+package org.cryptonit.cloud.timestamping;
+
+import org.cryptonit.cloud.ExecutionContext;
+
+public class PolicyFactory {
+    ExecutionContext context;
+
+    public PolicyFactory(ExecutionContext context) {
+        this.context = context;
+    }
+}
