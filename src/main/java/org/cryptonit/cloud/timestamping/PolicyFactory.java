c6c2e84ed1f6957f476d9feba0d6351a6a1c2420
Mathias Brossard <mathias@brossard.org>
2017-04-11 20:17:42 -0700

    PolicyFactory implements TimestampingPolicyFactory

--- src/main/java/org/cryptonit/cloud/timestamping/PolicyFactory.java
+++ src/main/java/org/cryptonit/cloud/timestamping/PolicyFactory.java
@@ -1,11 +1,24 @@
 package org.cryptonit.cloud.timestamping;
 
+import org.bouncycastle.asn1.ASN1ObjectIdentifier;
 import org.cryptonit.cloud.ExecutionContext;
+import org.cryptonit.cloud.interfaces.TimestampingPolicy;
+import org.cryptonit.cloud.interfaces.TimestampingPolicyFactory;
 
-public class PolicyFactory {
+public class PolicyFactory implements TimestampingPolicyFactory {
     ExecutionContext context;
 
     public PolicyFactory(ExecutionContext context) {
         this.context = context;
     }
+
+    @Override
+    public TimestampingPolicy getTimestampingPolicy(String domain) {
+        return null;
+    }
+
+    @Override
+    public TimestampingPolicy getTimestampingPolicy(String domain, ASN1ObjectIdentifier policyId) {
+        return null;
+    }    
 }
