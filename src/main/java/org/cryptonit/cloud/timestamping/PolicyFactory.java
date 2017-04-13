e75005d96faa8889252b6fffd8b3a37bd59533b1
Mathias Brossard <mathias@brossard.org>
2017-04-13 00:11:48 -0700

    Implement addTimestampPolicy methods

--- src/main/java/org/cryptonit/cloud/timestamping/PolicyFactory.java
+++ src/main/java/org/cryptonit/cloud/timestamping/PolicyFactory.java
@@ -1,5 +1,7 @@
 package org.cryptonit.cloud.timestamping;
 
+import java.sql.CallableStatement;
+import java.sql.Connection;
 import org.bouncycastle.asn1.ASN1ObjectIdentifier;
 import org.cryptonit.cloud.ExecutionContext;
 import org.cryptonit.cloud.interfaces.TimestampingPolicy;
@@ -24,9 +26,22 @@ public class PolicyFactory implements TimestampingPolicyFactory {
 
     @Override
     public void addTimestampingPolicy(TimestampingPolicy policy, String domain) throws Exception {
+        Connection c = context.database.getConnection();
+        CallableStatement cs = c.prepareCall("INSERT INTO timestamping_policy(domain, policyId, identityId, created) "
+                + "VALUES (?, NULL, ?, NOW())");
+        cs.setString(1, domain);
+        cs.setString(2, policy.getIdentity());
+        cs.execute();
     }
 
     @Override
     public void addTimestampingPolicy(TimestampingPolicy policy, String domain, ASN1ObjectIdentifier policyId) throws Exception {
+        Connection c = context.database.getConnection();
+        CallableStatement cs = c.prepareCall("INSERT INTO timestamping_policy(domain, policyId, identityId, created) "
+                + "VALUES (?, ?, ?, NOW())");
+        cs.setString(1, domain);
+        cs.setString(2, policyId.toString());
+        cs.setString(3, policy.getIdentity());
+        cs.execute();
     }
 }
