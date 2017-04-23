025dcaba161fae36494063e7a974bf0fe976d487
Mathias Brossard <mathias@brossard.org>
2017-04-23 02:52:23 -0700

    Missed one file in interfaces

--- src/main/java/org/cryptonit/cloud/interfaces/IdentityStore.java
+++ src/main/java/org/cryptonit/cloud/interfaces/IdentityStore.java
@@ -6,12 +6,15 @@ import javax.security.cert.X509Certificate;
 import org.bouncycastle.asn1.x500.X500Name;
 import org.bouncycastle.pkcs.PKCS10CertificationRequest;
 
+/**
+ * @author Mathias Brossard
+ */
 public interface IdentityStore {
 
     public String newIdentity(String domain, String keyId, X500Name subject) throws Exception;
 
     public boolean setCertificate(String domain, String identityId, X509Certificate certificate) throws Exception;
-    
+
     public PKCS10CertificationRequest getRequest(String domain, String identityId) throws Exception;
 
     public X509Certificate getCertificate(String domain, String identityId) throws Exception;
