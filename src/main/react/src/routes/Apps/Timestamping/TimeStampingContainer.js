37af0384548c5a9c29527b063bf5a2881a65c230
Mathias Brossard <mathias@brossard.org>
2017-05-05 01:54:24 -0700

    Add missing imports

--- src/main/react/src/routes/Apps/Timestamping/TimeStampingContainer.js
+++ src/main/react/src/routes/Apps/Timestamping/TimeStampingContainer.js
@@ -1,6 +1,14 @@
 import React from 'react';
+
+import {
+    Row,
+    Col,
+} from 'components';
+
 import { RoutedComponent, connect } from 'routes/routedComponent';
 import { CONTENT_VIEW_STATIC } from 'layouts/MainLayout/modules/layout';
+
+import Authorities from 'routes/Apps/timestamping/Authorities'
 
 class TimeStampingContainer extends RoutedComponent {
     getLayoutOptions() {
