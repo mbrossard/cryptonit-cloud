a2e8fe6d8e14f49ec9ccb289cba7d312c360bb00
Mathias Brossard <mathias@brossard.org>
2017-05-05 00:55:52 -0700

    Adding implementation details to timestamping Authorities

--- src/main/react/src/routes/Apps/Timestamping/Authorities.js
+++ src/main/react/src/routes/Apps/Timestamping/Authorities.js
@@ -1,28 +1,62 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+
+import {
+    Table
+} from 'components';
 
 export default class Authorities extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      authorities: []
+    };
+  }
+
+  componentDidMount() {
+  }
+
   render() {
     return (
     <Table>
       <thead>
         <tr>
-          <th></th>
-          <th></th>
-          <th></th>
-          <th></th>
-          <th></th>
+          <th>id</th>
+          <th>domain</th>
+          <th>subject</th>
+          <th>policy</th>
+          <th>actions</th>
         </tr>
       </thead>
       <tbody>
-        {this.props.authorities.map((tsa, i) =>
+        {this.state.authorities.map((authorities, i) =>
+        <tr>
+            <td>
+                <span>
+                  {authorities.id}
+                </span>
+              </td>
+              <td>
+                <span>
+                  {authorities.domain}
+                </span>
+              </td>
+              <td>
+                <span className='text-white'>
+                  {authorities.subject}
+                </span>
+              </td>
+              <td>
+                <span>
+                  {authorities.policy}
+                </span>
+              </td>
+              <td>
+              </td>
+            </tr>
         )}
       </tbody>
       </Table>
     )
   }
-}
-
-Authorities.propTypes = {
-  authorities: PropTypes.array.isRequired
-}
+};
