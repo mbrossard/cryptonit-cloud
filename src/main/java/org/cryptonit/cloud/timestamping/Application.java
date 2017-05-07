ee49cb5055d2d2f242cc584e8ecda8429264ccb9
Mathias Brossard <mathias@brossard.org>
2017-05-07 01:41:22 -0700

    Other missing import

--- src/main/java/org/cryptonit/cloud/timestamping/Application.java
+++ src/main/java/org/cryptonit/cloud/timestamping/Application.java
@@ -3,6 +3,7 @@ package org.cryptonit.cloud.timestamping;
 import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
