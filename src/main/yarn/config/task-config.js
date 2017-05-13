7b25fc42e35b565af16258bbc41d02dbb7967c05
Mathias Brossard <mathias@brossard.org>
2017-05-12 23:30:26 -0700

    Add loaders for style

--- src/main/yarn/config/task-config.js
+++ src/main/yarn/config/task-config.js
@@ -1,18 +1,76 @@
+const cssModulesLoader = {
+  loader: 'css-loader',
+  options: {
+    sourceMap: true,
+    minimize: false,
+    modules: true,
+    importLoaders: 1,
+    localIdentName: '[name]__[local]___[hash:base64:5]'
+  }
+}
+
+const sass_loaders = [
+  'style-loader',
+  cssModulesLoader,
+  {
+    loader: 'sass-loader',
+    options: {
+      sourceMap: true
+    }
+  }
+]
+
+const css_loaders = [
+  'style-loader',
+  cssModulesLoader,
+]
+
 module.exports = {
   html        : true,
   images      : true,
   fonts       : true,
-  static      : true,
-  svgSprite   : true,
-  ghPages     : true,
+  static      : false,
+  svgSprite   : false,
+  ghPages     : false,
   stylesheets : true,
 
   javascripts: {
+    extensions: ["js", "json", "scss", "jpg", "png"],
+
+    customizeWebpackConfig: function (webpackConfig, env, webpack) {
+      console.log(webpackConfig)
+      return webpackConfig
+    },
+
+    extractSharedJs: false,
+
     entry: {
-      // files paths are relative to
-      // javascripts.dest in path-config.json
       app: ["./app.js"]
     },
+
+    loaders: [
+      {
+        test: /\.scss$/,
+        loader: sass_loaders
+      },
+      {
+        test: /\.css$/,
+        loader: css_loaders
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      },
+      {
+        test: /\.(png|jpg)$/,
+        loader: {
+          loader: 'url-loader',
+          options: {
+            limit: 8192
+          }
+        }
+      }
+    ]
   },
 
   browserSync: {
