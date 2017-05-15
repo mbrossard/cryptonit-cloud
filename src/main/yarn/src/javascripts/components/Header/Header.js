fb62e426cb28e15ba339f232681a2cd669926d01
Mathias Brossard <mathias@brossard.org>
2017-05-15 00:16:14 -0700

    Add breadcrumbs in header

--- src/main/yarn/src/javascripts/components/Header/Header.js
+++ src/main/yarn/src/javascripts/components/Header/Header.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import _ from 'underscore';
+import { LinkContainer } from 'react-router-bootstrap';
 import {
     Grid,
     Row,
-    Col
+    Col,
+    Breadcrumb
 } from 'react-bootstrap';
 
 import { Colors } from 'consts';
@@ -12,33 +15,47 @@ import ROUTES_STRUCTURE, { findActiveNodes } from 'routes/structure';
 
 import classes from './Header.scss';
 
-const header = (title, path, containerFluid, children) => {
+const header = (title, path, containerFluid, children) => {
   return (
-        <div className="sub-navbar sub-navbar__header">
+        <div className="sub-navbar sub-navbar__header-breadcrumbs">
             <Grid fluid={ containerFluid }>
                 <Row>
-                    <Col lg={ 12 }>
-                        <div className={ classes.simpleHeaderWrap }>
-                            <Row>
-                                <Col xs={ children ? 6 : 12 }>
-                                    <div className={ classes.headerPart }>
-                                        <h1>
-                                            { title }
-                                        </h1>
-                                    </div>
-                                </Col>
-                                {
-                                    children && (
-                                        <Col xs={ 6 }>
-                                            <div className={ classes.headerPart }>
-                                                { children }
-                                            </div>
-                                        </Col>
-                                    )
-                                }
-                            </Row>
+                    <Col xs={ children ? 6 : 12 } className='sub-navbar-column'>
+                        <div className="sub-navbar-header">
+                            <h3>{ title || 'Home' }</h3>
                         </div>
+                        {
+                            (path && path.length > 0) ?
+                                (
+                                    <Breadcrumb
+                                        className='navbar-text navbar-right no-bg'
+                                    >
+                                        <LinkContainer to='/' onlyActiveOnIndex>
+                                            <Breadcrumb.Item active={ false }>
+                                                <i className="fa fa-fw fa-home"></i>
+                                            </Breadcrumb.Item>
+                                        </LinkContainer>
+                                        {
+                                            _.map(path.reverse(), (pathItem, index) => (
+                                                <Breadcrumb.Item
+                                                    href='javascript:void(0)'
+                                                    active={ index === path.length - 1 }
+                                                    key={ `header-breadcrumb-${index}` }
+                                                >{ pathItem.title }</Breadcrumb.Item>
+                                            ))
+                                        }
+                                    </Breadcrumb>
+                                ) : null
+                        }
+
                     </Col>
+                    {
+                        children && (
+                            <Col xs={ 6 }>
+                                { children }
+                            </Col>
+                        )
+                    }
                 </Row>
             </Grid>
         </div>
